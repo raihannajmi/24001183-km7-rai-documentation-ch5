fd8772fa09414f7a6f7300da952c37ee5efc9986
Najmi Raihan <najmiraihan@Najmis-MacBook-Pro.local>
2024-10-25 09:52:29 +0700

    feat: add function for get all user and add pagination

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const { Users } = require("../models");
+const { Op } = require("sequelize");
 
 const findUsers = async (req, res, next) => {
   try {
@@ -55,6 +56,60 @@ const updateUser = async (req, res, next) => {
   } catch (err) {}
 };
 
+const getAllUser = async (req, res) => {
+  try {
+    const { name, age, address, role, page = 1, perPage = 10 } = req.query;
+
+    const condition = {};
+    if (name) condition.name = { [Op.iLike]: `%${name}%` };
+    if (age) condition.age = { [Op.iLike]: `%${age}%` };
+    if (address) condition.address = { [Op.iLike]: `%${address}%` };
+    if (role) condition.role = { [Op.iLike]: `%${role}%` };
+
+    const limit = parseInt(perPage, 10);
+    const offset = (parseInt(page, 10) - 1) * limit;
+
+    const { count, rows: shops } = await Users.findAndCountAll({
+      where: condition,
+      limit,
+      offset,
+    });
+
+    const totalData = count;
+    const totalPages = Math.ceil(totalData / limit);
+
+    res.status(200).json({
+      data: shops,
+      meta: {
+        totalData,
+        page: parseInt(page, 10),
+        perPage: limit,
+        totalPages,
+        statusCode: 200,
+        message: "Data was successfully retrieved!",
+      },
+    });
+  } catch (error) {
+    console.log(error.name);
+    if (error.name === "SequelizeValidationError") {
+      const errorMessage = error.errors.map((err) => err.message);
+      return res.status(400).json({
+        status: "Fail",
+        message: errorMessage[0],
+        isSuccess: false,
+        data: null,
+      });
+    }
+
+    res.status(500).json({
+      status: "Fail",
+      message: error.message,
+      isSuccess: false,
+      data: null,
+    });
+  }
+};
+
 const deleteUser = async (req, res, next) => {
   try {
     const user = await Users.findOne({
@@ -81,4 +136,5 @@ module.exports = {
   findUserById,
   updateUser,
   deleteUser,
+  getAllUser,
 };
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
 
-const authController = require("../controllers/authController");
+// const authController = require("../controllers/authController");
+const userController = require("../controllers/userController");
 
 // router.post("", shopController.createShop);
-// router.get("", shopController.getAllShop);
+router.get("/", userController.getAllUser);
 // router.get("/:id", shopController.getShopById);
 // router.patch("/:id", shopController.updateShop);
 // router.delete("/:id", shopController.deleteShop);
